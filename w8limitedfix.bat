0a7826149cc8d97dd1e5e96dfadff8273dfc8a06
Vapor <31005724+DefinitelyVapor@users.noreply.github.com>
2017-08-14 19:25:32 -0400

    💻 Updated CMD window so that it stays open after file is executed 💻

--- w8limitedfix.bat
+++ w8limitedfix.bat
@@ -1,4 +1,6 @@
-netsh int ip reset C:\resetlog.txt
+@echo off
+netsh int ip reset Cresetlog.txt
 netsh int tcp set heuristics disabled
 netsh int tcp set global autotuninglevel=disabled
 netsh int tcp set global rss=enabled
+pause>nul
