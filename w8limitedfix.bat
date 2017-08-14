799f416cbaa64961373a1e0c1b159281776ac155
Vapor <31005724+DefinitelyVapor@users.noreply.github.com>
2017-08-14 19:29:51 -0400

    💻 Fixed errors and bugs 💻
    Make sure to run the .bat file as Administrator though! ^_^

--- w8limitedfix.bat
+++ w8limitedfix.bat
@@ -1,5 +1,5 @@
 @echo off
-netsh int ip reset Cresetlog.txt
+netsh int ip reset C:\resetlog.txt
 netsh int tcp set heuristics disabled
 netsh int tcp set global autotuninglevel=disabled
 netsh int tcp set global rss=enabled
